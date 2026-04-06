649dcf91d367f21613624d5949b482b7b060eb17
Rowan Noel <87455207+rowannoel@users.noreply.github.com>
2026-04-06 11:13:05 -0400

    Update materialized_cube.sql

--- hw12_olap_cube/materialized_cube.sql
+++ hw12_olap_cube/materialized_cube.sql
@@ -9,14 +9,17 @@ JOIN Product p ON s.productId = p.productId
 JOIN Dealer d ON s.dealerId = d.dealerId
 GROUP BY CUBE (p.productName, d.dealerName, s.saleDate);
 
---Refresh
+
+-- insert new data
 INSERT INTO Sales (productId, dealerId, saleDate, price) VALUES
 (1, 3, '2025-04-02', 1400.00),
 (2, 2, '2025-04-05', 540.00),
 (3, 1, '2025-04-07', 930.00);
 
+-- refresh cube
 REFRESH MATERIALIZED VIEW sales_cube_mv;
 
+-- view updated cube
 SELECT *
 FROM sales_cube_mv
 ORDER BY productName, dealerName, saleDate;
