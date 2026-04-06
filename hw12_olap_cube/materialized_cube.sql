bbf543db810ec3a7fe7de12b3dbd521327390f96
Rowan Noel <87455207+rowannoel@users.noreply.github.com>
2026-04-06 11:12:40 -0400

    Create materialized_cube.sql

---
+++ hw12_olap_cube/materialized_cube.sql
@@ -0,0 +1,22 @@
+CREATE MATERIALIZED VIEW sales_cube_mv AS
+SELECT
+    p.productName,
+    d.dealerName,
+    s.saleDate,
+    SUM(s.price) AS totalSales
+FROM Sales s
+JOIN Product p ON s.productId = p.productId
+JOIN Dealer d ON s.dealerId = d.dealerId
+GROUP BY CUBE (p.productName, d.dealerName, s.saleDate);
+
+--Refresh
+INSERT INTO Sales (productId, dealerId, saleDate, price) VALUES
+(1, 3, '2025-04-02', 1400.00),
+(2, 2, '2025-04-05', 540.00),
+(3, 1, '2025-04-07', 930.00);
+
+REFRESH MATERIALIZED VIEW sales_cube_mv;
+
+SELECT *
+FROM sales_cube_mv
+ORDER BY productName, dealerName, saleDate;
